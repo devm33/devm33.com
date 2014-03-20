c632351447fdbf7c3b47e745751b0710116eb510
Devraj Mehta <devm33@gmail.com>
2014-03-20 15:21:16 -0400

    moved yuicompressor jar to util and added link to gist -- website changes pushed live Thu Mar 20 15:21:16 EDT 2014

--- gen_index.pl
+++ gen_index.pl
@@ -9,7 +9,7 @@ use warnings;
 my $cssfile = 'style.css';
 my $htmlfile = 'template.index.html';
 my $jsfile = 'inline.js';
-my $yuic = 'java -jar /home/devraj/Code/yuicompressor/yuicompressor.jar';
+my $yuic = 'java -jar ./util/yuicompressor.jar';
 my $outfile = 'index.html';
 my %map = ();
 my ($in, $out, $html);
