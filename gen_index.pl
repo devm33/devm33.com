fb12c1c0aa515fa88ff15525ac3a6b11fd36b235
Devraj Mehta <devm33@gmail.com>
2014-03-21 00:25:46 -0400

    used some perl magic to automate text for webfont optimization -- website changes pushed live Fri Mar 21 01:10:40 EDT 2014

--- gen_index.pl
+++ gen_index.pl
@@ -9,7 +9,7 @@ use warnings;
 my $cssfile = 'style.css';
 my $htmlfile = 'template.index.html';
 my $jsfile = 'inline.js';
-my $yuic = 'java -jar ./util/yuicompressor-2.4.8.jar';
+my $yuic = 'java -jar util/yuicompressor-2.4.8.jar';
 my $outfile = 'index.html';
 my %map = ();
 my ($in, $out, $html);
@@ -17,6 +17,7 @@ my ($in, $out, $html);
 # minify
 $map{"CSS"} = `$yuic $cssfile`;
 $map{"JS"} = `$yuic $jsfile`;
+$map{"WEBFONTCHARS"} = grabchars();
 
 # compile
 open $in, '<', $htmlfile or die "error opening $htmlfile: $!";
@@ -30,3 +31,31 @@ close $out or die $!;
 
 # compress
 system("gzip -c --best --force $outfile > ${outfile}.gz");
+
+# subroutine to grab the text that will use a webfont in html file
+sub grabchars {
+    use HTML::TreeBuilder::XPath;
+    
+    my $fontclass = 'webfont'; # name of the trigger class
+
+    # parse html file into XPath tree
+    my $tree = HTML::TreeBuilder::XPath->new;
+    $tree->parse_file($htmlfile);
+
+    # grab the text of all subnodes of tags with the class
+    my $text = '';
+    for my $node ($tree->findnodes('//*[@class="' . $fontclass . '"]')) {
+        $text .= $node->findvalue('./*');
+    }
+
+    # done with html, clean up
+    $tree->delete;
+
+    # remove whitespace - webfont api would break / doesnt need them
+    $text =~ s/\s//g;
+
+    # remove repeated characters with a little perl regex love
+    1 while($text =~ s/(\w+)(.*)\1/$1$2/g);
+
+    return $text;
+}
