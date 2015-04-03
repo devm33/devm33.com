bdcc45f03bcd4af602f478d7eb46e252498673eb
Devraj Mehta <devm33@gmail.com>
2015-04-03 02:36:53 -0400

    added console log message

---
+++ ln/_assets/js/consolelog.js
@@ -0,0 +1,10 @@
+(console||{}).log(
+"Hello,\n" +
+"\n" +
+"Thank you for your interest! This website is kept open source on github:\n" +
+"\n" +
+"https://github.com/devm33/devm33\n" +
+"\n" +
+"Cheers,\n" +
+"Devraj"
+);
