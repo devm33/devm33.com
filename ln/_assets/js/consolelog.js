a2ecd5718c4ca4a015cbb6c47bb9e8a728923f19
Devraj Mehta <devm33@gmail.com>
2016-03-06 22:30:13 -0500

    whoops thats a bug

--- ln/_assets/js/consolelog.js
+++ ln/_assets/js/consolelog.js
@@ -1,10 +1,12 @@
-(console||{}).log(
-"Hello,\n" +
-"\n" +
-"Thank you for your interest! This website is kept open source on github:\n" +
-"\n" +
-"https://github.com/devm33/devm33\n" +
-"\n" +
-"Cheers,\n" +
-"Devraj"
-);
+if (console && console.log) {
+  console.log(
+      "Hello,\n" +
+      "\n" +
+      "Thank you for your interest! This website is kept open source on github:\n" +
+      "\n" +
+      "https://github.com/devm33/devm33\n" +
+      "\n" +
+      "Cheers,\n" +
+      "Devraj"
+      );
+}
