f70d0b93e7b264977e79d4248d9a69691eade44c
Devraj Mehta <devm33@gmail.com>
2019-05-27 22:45:20 -0400

    fix version warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,11 @@ module.exports = {
   extends: ["google", "prettier", "plugin:react/recommended"],
   plugins: ["react", "jsx-a11y", "import"],
   parser: "babel-eslint",
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
   env: {
     browser: true
   }
