d76dc6a56571dd437186399e98f32c60082c7045
Devraj Mehta <devm33@gmail.com>
2019-05-30 21:38:31 -0400

    styling in progress

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,19 +7,87 @@ import Layout from "../components/layout";
 
 const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   width: 100%;
 `;
 
-const Card = styled.div``;
+const Card = styled.div`
+  position: relative;
+  &:before {
+    content: "";
+    display: block;
+    padding-top: 100%;
+  }
+`;
 
-const IndexPage = ({ data }) => (
+const ImageWrapper = styled.div`
+  > div {
+    height: 100%;
+    left: 0;
+    position: absolute !important;
+    top: 0;
+    width: 100%;
+    > div {
+      position: static !important;
+    }
+  }
+`;
+
+const Overlay = styled.div`
+  height: 100%;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+  background: linear-gradient(
+    0deg,
+    rgba(255, 255, 255, 0.9) 0%,
+    rgba(255, 255, 255, 0) 100%
+  );
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+const Title = styled.span`
+  font-size: 2rem;
+  padding: 1rem 0;
+  text-align: center;
+`;
+
+const Content = styled.div`
+  height: 100%;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+
+  a:hover {
+    ${Overlay} {
+      background: rgba(255, 255, 255, 0.7);
+    }
+  }
+`;
+
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { nodes }
+  }
+}) => (
   <Layout>
     <Wrapper>
-      {data.allMarkdownRemark.nodes.map(node => (
+      {nodes.map(node => (
         <Card key={node.fields.path}>
-          <Img fluid={node.frontmatter.image.childImageSharp.fluid} />
-          <Link to={node.fields.path}>{node.frontmatter.title}</Link>
+          <Content>
+            <Link to={node.fields.path}>
+              <ImageWrapper>
+                <Img fluid={node.frontmatter.image.childImageSharp.fluid} />
+              </ImageWrapper>
+              <Overlay>
+                <Title>{node.frontmatter.title}</Title>
+              </Overlay>
+            </Link>
+          </Content>
         </Card>
       ))}
     </Wrapper>
@@ -41,12 +109,8 @@ export const query = graphql`
           updated
           image {
             childImageSharp {
-              fluid(
-                maxWidth: 850
-                quality: 90
-                traceSVG: { color: "#f3f3f3" }
-              ) {
-                ...GatsbyImageSharpFluid_withWebp_tracedSVG
+              fluid(maxWidth: 500, maxHeight: 500) {
+                ...GatsbyImageSharpFluid_withWebp
               }
             }
           }
