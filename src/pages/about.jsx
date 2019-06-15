3cc462ad7fcb6d1d68f8a1023eac4018c1905eb3
Devraj Mehta <devm33@gmail.com>
2019-06-14 21:23:47 -0400

    add link to repo

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,6 +6,12 @@ const About = () => (
   <Layout>
     <h1>TODO</h1>
     <p>TODO: Create about page</p>
+    <p>
+      This blog&apos;s source:{" "}
+      <a href="https://github.com/devm33/devm.dev">
+        https://github.com/devm33/devm.dev
+      </a>
+    </p>
   </Layout>
 );
 
