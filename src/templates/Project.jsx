ba1396b7954e8b40182b0f371daf5f8e52258cb6
Devraj Mehta <devm33@gmail.com>
2019-06-18 20:34:57 -0400

    fix code font size

--- src/templates/Project.jsx
+++ src/templates/Project.jsx
@@ -4,7 +4,8 @@ import styled from "styled-components";
 import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 
 import "katex/dist/katex.min.css";
-import "prismjs/themes/prism-solarizedlight.css";
+// import "prismjs/themes/prism-solarizedlight.css";
+import "prismjs/themes/prism.css";
 
 import theme from "../theme";
 import { rhythm } from "../typography";
