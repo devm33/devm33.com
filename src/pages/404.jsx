b0bf036db81b256923455a6f9684f163bab6d46c
Devraj Mehta <devm33@gmail.com>
2019-08-18 16:30:40 -0400

    add margin to 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 
+import theme from "../theme";
 import Layout from "../components/Layout";
+
+const Article = styled.article`
+  max-width: ${theme.contentWidth}px;
+  width: 80%;
+  margin: 0 auto;
+`;
 
 const NotFoundPage = () => (
   <Layout title="404: Not found" url="/404.html">
-    <h1>Resource not found</h1>
-    <p>Sorry this resource was not found.</p>
+    <Article>
+      <h1>Resource not found</h1>
+      <p>Sorry this resource was not found.</p>
+    </Article>
   </Layout>
 );
 
