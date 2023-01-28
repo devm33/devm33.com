e20adbfa6e87bf91a6ac7986442a8a4cc907223a
Devraj Mehta <devm33@gmail.com>
2023-01-27 23:10:56 -0500

    update resume, swtich index and about pages

---
+++ src/pages/projects.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { graphql } from "gatsby";
+
+import Layout from "../components/Layout";
+import ProjectGrid from "../components/ProjectGrid";
+
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { nodes },
+  },
+}) => (
+  <Layout url="/projects/">
+    <ProjectGrid nodes={nodes} />
+  </Layout>
+);
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark(
+      filter: { fields: { type: { eq: "projects" } } }
+      sort: { fields: frontmatter___updated, order: DESC }
+    ) {
+      nodes {
+        ...ProjectGridFields
+      }
+    }
+  }
+`;
+
+export default IndexPage;
