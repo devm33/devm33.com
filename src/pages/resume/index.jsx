2ca2449e4bc430b423b60da897906b654c6dd601
Devraj Mehta <devm33@gmail.com>
2023-01-27 21:56:47 -0500

    update resume

--- src/pages/resume/index.jsx
+++ src/pages/resume/index.jsx
@@ -7,60 +7,74 @@ import "../../reset.css";
 
 const Wrapper = styled.div`
   @import url('https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400;0,500;1,400&display=swap');
-  font-family: 'Arimo', sans-serif;
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;1,400&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,400;0,500;1,400&display=swap');
+  font-family: 'Mulish', sans-serif;
   font-size: 14px;
+  margin: 1em auto 10em auto;
   max-width: 682px;
-  margin: 10px auto 100px auto;
+  line-height: 1.4;
   @media print {
     @page {
       margin: 0;
     }
-    margin-top: 20px;
     margin-bottom: 0;
+    margin-top: 2em;
   }
   a,
   a:visited {
-    color: #00e;
+    color: #1976d2;
   }
   h1 {
-    font-size: 1.2em;
+    font-size: 1.5em;
+    letter-spacing: 0.5px;
   }
-  h4 {
-    margin: 10px 0 5px 0;
+  h2 {
+    font-size: 1em;
+    margin: 1em 0 0.5em 0;
   }
-  table {
-    width: 100%;
+  h3 {
+    font-size: 1em;
+  }
+  ul {
+    padding-inline-start: 20px;
+  }
+  li {
+    list-style-type: circle;
   }
 `;
 
 const Header = styled.header`
+  align-items: center;
   display: flex;
-  align-items: baseline;
-  justify-content: space-around;
+  justify-content: space-between;
 `;
 
-const H1 = styled.h1`
-  letter-spacing: 0.5px;
+const ContactInfo = styled.div`
+  display: flex;
+  gap: 1em;
 `;
 
-const TrMeta = styled.tr`
-  & > td:not(:first-child) {
-    padding-left: 5px;
-  }
+const TitleRow = styled.div`
+  display: flex;
+  margin: 0.5em 0 0.1em 0;
 `;
-const TrDesc = styled.tr`
-  & > td {
-    padding-top: 5px;
-    padding-bottom: 10px;
-    line-height: 1.35em;
-  }
+
+const Location = styled.span`
+  color: #666;
+  font-style: italic;
+  font-weight: normal;
+  white-space: nowrap;
+`
+
+const DateRange = styled.div`
+  color: #666;
+  font-size: 0.85em;
+  margin-left: auto;
 `;
-const TdRight = styled.td`
-  text-align: right;
-`;
-const TdJustify = styled.td`
-  text-align: justify;
-  text-align-last: justify;
+
+const DateSpan = styled.span`
+  white-space: nowrap;
 `;
 
 const NoPrint = styled.div`
@@ -70,9 +84,9 @@ const NoPrint = styled.div`
 `;
 
 const TopLeft = styled(NoPrint)`
+  left: 30px;
   position: fixed;
   top: 30px;
-  left: 30px;
   @media (max-width: 950px) {
     display: none;
   }
@@ -95,82 +109,65 @@ const Resume = ({
   <Wrapper>
     <Meta title="Devraj Mehta Resume" url="/resume/" image={src} />
     <Header>
-      <H1>
-        <a href="https://www.linkedin.com/in/devrajmehta">Devraj Mehta</a>
-      </H1>
-      <h4>
+      <h1>
+        Devraj Mehta
+      </h1>
+      <ContactInfo>
+        <a href="https://www.linkedin.com/in/devrajmehta">linkedin.com/in/devrajmehta</a>
         <a href={`mailto:${email}`}>{email}</a>
-      </h4>
+      </ContactInfo>
     </Header>
-    <table>
-      <tbody>
-        <tr>
-          <td colSpan="4">
-            <h4>Work Experience</h4>
-          </td>
-        </tr>
-        {jobs.map(job => (
-          <Fragment key={job.id}>
-            <TrMeta>
-              <td>
-                <a href={job.uri}>{job.name}</a>
-              </td>
-              <td>{job.title}</td>
-              <td>
-                <i>{job.location}</i>
-              </td>
-              <TdRight>
-                {job.start} {job.finish && "– " + job.finish}
-              </TdRight>
-            </TrMeta>
-            <TrDesc>
-              <td colSpan="4">{job.description}</td>
-            </TrDesc>
-          </Fragment>
-        ))}
-        <tr>
-          <td colSpan="4">
-            <h4>Education</h4>
-          </td>
-        </tr>
-        <TrMeta>
-          <td>
-            <a href="https://gatech.edu">Georgia Institute of Technology</a>
-          </td>
-          <td>
-            BSc Computer Science
-          </td>
-          <td>
-            <i>Highest Honors</i>
-          </td>
-          <TdRight>2010 – 2014</TdRight>
-        </TrMeta>
-      </tbody>
-    </table>
-    <table>
-      <tbody>
-        <tr>
-          <td colSpan="4">
-            <h4>Skills</h4>
-          </td>
-        </tr>
-        {Object.keys(skills).map(category => (
-          <tr key={category}>
-            <td>
-              <i>{category}</i>
-            </td>
-            <TdJustify>{skills[category].join(", ")}</TdJustify>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+
+    <h2>EXPERIENCE</h2>
+    {jobs.filter(job => job.enabled).map(job => (
+      <div key={job.id}>
+        <TitleRow>
+          <h3>
+            <a href={job.uri}>{job.name}</a>, {job.title}{' '}
+            <Location>- {job.location}</Location>
+          </h3>
+          <DateRange>
+            <DateSpan>{job.start}</DateSpan> - <DateSpan>{job.finish}</DateSpan>
+          </DateRange>
+        </TitleRow>
+        <ul>
+          {job.description.map(desc => (
+            <li>{desc}</li>
+          ))}
+        </ul>
+      </div>
+    ))}
+
+    <h2>EDUCATION</h2>
+    <TitleRow>
+      <h3>
+        <a href="https://gatech.edu">Georgia Institute of Technology</a>,{' '}
+        BSc Computer Science{' '}
+        <Location>- Atlanta, GA</Location>
+      </h3>
+      <DateRange>
+        <DateSpan>AUG 2010</DateSpan> - <DateSpan>MAY 2014</DateSpan>
+      </DateRange>
+    </TitleRow>
+    <div>
+      Highest Honors
+    </div>
+
+    <h2>SKILLS</h2>
+    {
+      Object.keys(skills).map(category => (
+        <div key={category}>
+          {skills[category].join(", ")}
+        </div>
+      ))
+    }
     <TopLeft>
       <Link to="/about/">&larr; Back to about</Link>
       <br />
       <br />
       <a href="/resume.pdf">PDF Version</a>
     </TopLeft>
-  </Wrapper>
+  </Wrapper >
 );
 
 export default Resume;
@@ -188,6 +185,7 @@ export const query = graphql`
         title
         start
         finish
+        enabled
       }
     }
     resumeYaml {
