94cdb8083463fc109b84ba62d959d6cc2684fb32
Devraj Mehta <devm33@gmail.com>
2023-02-20 14:33:14 -0500

    Add cache for new puppeteer location on netlify

---
+++ plugins/keep-data-cache/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // Restore file/directory cached in previous builds.
+  // Does not do anything if:
+  //  - the file/directory already exists locally
+  //  - the file/directory has not been cached yet
+  async onPreBuild({ utils }) {
+    // for Netlify Puppeteer
+    await utils.cache.restore('/opt/buildhome/.cache/puppeteer/');
+  },
+  // Cache file/directory for future builds.
+  // Does not do anything if:
+  //  - the file/directory does not exist locally
+  async onPostBuild({ utils }) {
+    // for Netlify Puppeteer
+    await utils.cache.save('/opt/buildhome/.cache/puppeteer/');
+  }
+};
