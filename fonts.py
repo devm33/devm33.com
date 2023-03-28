04798369d36001119a1676cecbcf4a70c5b1d39d
Devraj Mehta <devm33@gmail.com>
2023-03-28 16:41:07 -0400

    No-op: changed the sort order of the characters in this script

--- fonts.py
+++ fonts.py
@@ -8,8 +8,8 @@ font = 'family=Mulish:ital,wght@0,400'
 # chars = ''.join([chr(i) for i in range(0X021, 0X07E)])
 
 # Obtained from:
-# document.querySelector('.resume__Wrapper-sc-1vort98-0').textContent.replace(/\s+/g, '')
-chars = 'DevrajMhtlinkd.com/@EXPRINCGg,TL-sbuA201SBpfy+z5wxUq9$3%YOV7()46F&JH!WKQ←'
+# Array.from(new Set(Array.from(document.querySelector('.resume__Wrapper-sc-1vort98-0').textContent.replace(/\s+/g, '')))).sort().join('')
+chars = '!$%&()+,-./012345679@ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyz←'
 
 
 print(api + '?' + font + '&display=block&text=' + urllib.parse.quote(chars))
