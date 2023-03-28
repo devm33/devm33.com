e0b7b31497bf7346adcb4f921fce0670a96c3c09
Devraj Mehta <devm33@gmail.com>
2023-03-28 12:00:31 -0400

    Trim a kb off the webfont by limiting character set to what is on the page

--- fonts.py
+++ fonts.py
@@ -3,9 +3,15 @@
 import urllib.parse
 
 api = 'https://fonts.googleapis.com/css2'
-font = 'family=Mulish:ital,wght@0,400..500;1,400'
-# font = 'family=Mulish:ital,wght@0,400'
-chars = ''.join([chr(i) for i in range(0X021, 0X07E)])
+# font = 'family=Mulish:ital,wght@0,400..500;1,400'
+font = 'family=Mulish:ital,wght@0,400'
+# chars = ''.join([chr(i) for i in range(0X021, 0X07E)])
+
+# Obtained from:
+# document.querySelector('.resume__Wrapper-sc-1vort98-0').textContent.replace(/\s+/g, '')
+chars = 'DevrajMhtlinkd.com/@EXPRINCGg,TL-sbuA201SBpfy+z5wxUq9$3%YOV7()46F&JH!WKQ←'
+
+
 print(api + '?' + font + '&display=block&text=' + urllib.parse.quote(chars))
 
 
