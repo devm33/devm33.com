3c160acabef3493eb431edea2c27e1fa29af3d1c
Devraj Mehta <devm33@gmail.com>
2023-04-03 22:28:46 -0400

    Remove unused styles from layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,36 +1,17 @@
 import PropTypes from "prop-types";
 import React from "react";
-import styled from "styled-components";
 
 import "../global.css";
-import theme from "../theme";
 import { InstallIcons } from "./Icons";
 import { Navbar } from "./Navbar";
 
-const Wrapper = styled.div`
-  color: ${theme.fg};
-  background-color: ${theme.bg};
-  a,
-  a:visited {
-    color: ${theme.link};
-  }
-  a:hover {
-    color: ${theme.accent};
-  }
-  a {
-    text-decoration: none;
-    transition: all 0.3s ease-in-out; /* all prop is overridden below */
-    transition-property: color, background-color, border-color;
-  }
-`;
-
 /** Layout component to serve as the base for all pages.  */
 export const Layout = ({ children }) => (
-  <Wrapper>
+  <>
     <InstallIcons github={true} link={true} linkedin={true} />
     <Navbar />
     <main>{children}</main>
-  </Wrapper>
+  </>
 );
 
 Layout.propTypes = {
