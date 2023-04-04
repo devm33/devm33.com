39627434e8fe4d3935db94384a82b976e50d490d
Devraj Mehta <devm33@gmail.com>
2023-04-04 13:55:43 -0400

    Modularize tag template css

--- src/templates/Tag.jsx
+++ src/templates/Tag.jsx
@@ -1,48 +1,24 @@
 import { graphql } from "gatsby";
-import PropTypes from "prop-types";
 import React from "react";
-import styled from "styled-components";
 
 import { Layout } from "../components/Layout";
+import { pill } from "../components/Pill.module.css";
 import ProjectGrid from "../components/ProjectGrid";
-import theme from "../theme";
-import { rhythm } from "../typography";
+import { title, titlePill } from "./Tag.module.css";
 
 export { Head } from "../components/Head";
 
-const Header = styled.h1`
-  ${theme.font.title}
-  padding: ${rhythm(1 / 2)};
-  display: flex;
-`;
-
-const Tag = styled.span`
-  border-radius: 0.5em;
-  border: 1px solid ${theme.link};
-  color: ${theme.link};
-  padding: 0 0.5em;
-  margin-left: ${rhythm(1 / 4)};
-`;
-
-const TagTemplate = ({
-  pageContext: { tag },
-  data: {
-    allMarkdownRemark: { nodes },
-  },
-}) => (
-  <Layout>
-    <Header>
-      Projects tagged <Tag>{tag}</Tag>
-    </Header>
-    <ProjectGrid nodes={nodes} />
-  </Layout>
-);
-
-TagTemplate.propTypes = {
-  pageContext: PropTypes.shape({
-    tag: PropTypes.string.isRequired,
-  }),
-};
+export default function TagTemplate({ data, pageContext }) {
+  return (
+    <Layout>
+      <h1 className={title}>
+        Projects tagged
+        <span className={`${pill} ${titlePill}`}>{pageContext.tag}</span>
+      </h1>
+      <ProjectGrid nodes={data.allMarkdownRemark.nodes} />
+    </Layout>
+  );
+}
 
 export const query = graphql`
   query($tag: String!) {
@@ -59,5 +35,3 @@ export const query = graphql`
     }
   }
 `;
-
-export default TagTemplate;
