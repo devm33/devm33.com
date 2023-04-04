08a1315edf665ab913378fe197de457fd50c386a
Devraj Mehta <devm33@gmail.com>
2023-04-04 14:01:42 -0400

    Chew through these unused theme values, remove polished dep!

--- src/theme.js
+++ src/theme.js
@@ -1,21 +1,9 @@
-import { darken, lighten } from "polished";
 import { scale } from "./typography";
 
 export const theme = {
-  bg: "white",
-  color: "black",
-  secondary: lighten(0.3, "black"),
-  accent: "#d6006c",
-  link: darken(0.2, "cornflowerblue"),
-  break: {
-    xs: "(max-width: 300px)",
-    s: "(max-width: 500px)",
-  },
   font: {
     small: scale(-1 / 6),
-    body: scale(0),
     icon: scale(1 / 6),
-    title: scale(1 / 2),
   },
 };
 
