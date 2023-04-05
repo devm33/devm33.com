6e79fbaa40dfb844fbee0b2508b90a40f9bcfe9c
Devraj Mehta <devm33@gmail.com>
2023-04-05 12:43:08 -0400

    update chars - didnt change font result

--- fonts.py
+++ fonts.py
@@ -8,8 +8,8 @@ font = 'family=Mulish:ital,wght@0,400'
 # chars = ''.join([chr(i) for i in range(0X021, 0X07E)])
 
 # Obtained from:
-# Array.from(new Set(Array.from(document.querySelector('.resume__Wrapper-sc-1vort98-0').textContent.replace(/\s+/g, '')))).sort().join('')
-chars = '!$%&()+,-./012345679@ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyz←'
+# Array.from(new Set(Array.from(document.querySelector('main').textContent.replace(/\s+/g, '')))).sort().join('')
+chars = '!$%&()+,-./012345679@ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyz'
 
 
 print(api + '?' + font + '&display=block&text=' + urllib.parse.quote(chars))
