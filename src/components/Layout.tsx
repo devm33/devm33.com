33352c615456d7486f4369ad63e6db59c6a7bb51
Devraj Mehta <devm33@gmail.com>
2023-04-05 22:21:06 -0400

    Type layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,10 @@
-import PropTypes from "prop-types";
 import React from "react";
 
 import { InstallIcons } from "./Icons";
 import { Navbar } from "./Navbar";
 
 /** Layout component to serve as the base for all pages.  */
-export function Layout({ children }) {
+export function Layout({ children }: React.PropsWithChildren) {
   return (
     <>
       <InstallIcons github={true} link={true} linkedin={true} />
@@ -14,7 +13,3 @@ export function Layout({ children }) {
     </>
   );
 }
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-};
