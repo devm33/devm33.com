a2bfa5b16e3c0718600c1e4a6a91248e5e69364f
Devraj Mehta <devm33@gmail.com>
2023-04-21 14:22:31 -0400

    Firefox hammer was a bad call, add some flags for debugging

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -110,15 +110,14 @@ export const createPages: GatsbyNode["createPages"] = async (args) => {
 export const onPostBuild: GatsbyNode["onPostBuild"] = async () => {
   // Generate PDF of resume page
   const browser = await puppeteer.launch({
-    product: "firefox",
-    args: ["--font-render-hinting=none", "--disable-web-security"],
+    args: ["--font-render-hinting=none", "--enable-logging", "--no-sandbox"],
   });
   const page = await browser.newPage();
   const resumePath = path.join(__dirname, "public/resume/index.html");
-  await page.goto(url.pathToFileURL(resumePath).toString()); //, {
-  //waitUntil: "networkidle0", // cspell:disable-line
-  //});
-  // await page.evaluateHandle("document.fonts.ready");
+  await page.goto(url.pathToFileURL(resumePath).toString(), {
+    waitUntil: "networkidle0", // cspell:disable-line
+  });
+  await page.evaluateHandle("document.fonts.ready");
   await page.screenshot({ path: "./public/test.png" });
   await page.pdf({ path: "./public/devraj_mehta_resume.pdf" });
   await browser.close();
