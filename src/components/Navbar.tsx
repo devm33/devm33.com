51e9c401b366ffca4071219e07af0222f315da86
Devraj Mehta <devm33@gmail.com>
2023-04-21 23:28:31 -0400

    Remove projects link from navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,12 +16,9 @@ export function Navbar() {
   `);
   return (
     <nav className={css.navBar}>
-      <div>
-        <Link to="/projects/">Projects</Link>
-      </div>
-      <div className={css.title}>
-        <Link to="/">{site?.siteMetadata.title}</Link>
-      </div>
+      <Link to="/" className={css.title}>
+        {site?.siteMetadata.title}
+      </Link>
       <div className={css.iconLinks}>
         <a href="https://github.com/devm33" aria-label="GitHub profile">
           <GitHubIcon />
