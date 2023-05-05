ab476e7331e5f6f84ad784a9dcdd9a7377e5b84b
Devraj Mehta <devm33@gmail.com>
2023-05-04 21:30:03 -0400

    Fix ssr issue

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -7,37 +7,39 @@ interface Props {
   className?: string;
 }
 
-const isBrowser = typeof window !== "undefined";
-
-function updateTheme(light: boolean) {
+function updateThemeClass(light: boolean) {
   document.documentElement.classList.toggle("light", light);
   document.documentElement.classList.toggle("dark", !light);
-  if (isBrowser) window.localStorage.setItem("light", light ? "light" : "dark");
 }
 
-function getInitialLight(): boolean {
-  if (isBrowser) {
-    const localStorageLight = window.localStorage.getItem("light");
-    if (localStorageLight !== undefined) return localStorageLight === "light";
-  }
+function persistThemeSetting(light: boolean) {
+  localStorage.setItem("theme", light ? "light" : "dark");
+}
+
+function getInitialTheme(): boolean {
+  const localStorageLight = localStorage.getItem("theme");
+  if (localStorageLight !== undefined) return localStorageLight === "light";
   const darkMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
   return !darkMediaQuery.matches;
 }
 
 export function ThemeToggle({ className }: Props) {
-  const [light, setLight] = useState(getInitialLight());
+  const [light, setLight] = useState(true);
+  useEffect(() => setLight(getInitialTheme()), []); // Runs only once
+  useEffect(() => updateThemeClass(light), [light]);
 
-  useEffect(() => {
-    updateTheme(light);
-  }, [light]);
+  function toggle() {
+    persistThemeSetting(!light);
+    setLight(!light);
+  }
 
   const classes = [css.button, icon.link, className].filter(Boolean).join(" ");
   return (
     <button
-      aria-label="Toggle dark theme"
+      aria-label="Toggle theme"
       aria-pressed={!light}
       className={classes}
-      onClick={() => setLight(!light)}
+      onClick={toggle}
     >
       <Icon icon={Icons.Brightness} />
     </button>
